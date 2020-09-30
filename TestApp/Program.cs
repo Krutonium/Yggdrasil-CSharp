885830101f1a29e051a0595c3bb71c31af43b5e1
Krutonium <PFCKrutonium@gmail.com>
2020-09-30 18:26:29 -0400

    Basic Authentication Works, Pass 1 Complete. Pass 2 will add proper login handling

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Yggdrasil_CSharp;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Yggdrasil_CSharp.Yggdrasil yggdrasil = new Yggdrasil();
+            var Authenticated = yggdrasil.Login("Blarg", "Blarg");
+            if (Authenticated != null)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Yggdrasil-CSharp/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Yggdrasil_CSharp
+{
+    public class Yggdrasil
+    {
+        static readonly string APIBase = "https://authserver.mojang.com/";
+        private static readonly string AuthServer = APIBase + "/authenticate";
+        public string Login(string Username, string Password)
+        {
+            Payload payload = new Payload();
+            payload.username = Username;
+            payload.password = Password;
+
+            return (SendRequest(new Uri(AuthServer), JsonConvert.SerializeObject(payload)));
+        }
+        
+        private string SendRequest(Uri uri, string JSONData)
+        {
+            var JsonDataBytes = Encoding.UTF8.GetBytes(JSONData);
+            try
+            {
+                var Client = new WebClient();
+                Client.Headers.Add("Content-Type", "application/json");
+                byte[] Response = Client.UploadData(uri, "POST", JsonDataBytes);
+                var ReturnedData = Encoding.UTF8.GetString(Response);
+                return ReturnedData;
+            }
+            catch (WebException ex)
+            {
+                return null;
+            }
+        }
+        public class Agent {
+            public string name { get; set; } = "Minecraft";
+            public int version { get; set; } = 1;
+        }
+
+        public class Payload
+        {
+            public Agent agent { get; set; }
+            public string username { get; set; } 
+            public string password { get; set; }
+            public string clientToken { get; set; } = "Yggdrasil-CSharp";
+            public bool requestUser { get; set; } = true;
+        }
+    }
+}
